b362d588365273d79bdbef2c5e5725fe8c157bd5
MOHAMMEDALMASHHOR <u211118091@samsun.edu.tr>
2023-11-06 17:57:11 +0300

    The program is done 06/11/2023

---
+++ src/MathHelper.java
@@ -0,0 +1,14 @@
+public class MathHelper {
+    public static int add(int a, int b){
+        return a+b;}
+    public static int subtract(int a, int b){
+        return a+b;}
+    public static int multipy(int a, int b){
+        return a+b;}
+    public static int divide(int a, int b){
+        if (b==0) {
+            throw new ArithmeticException("Division by zero is not accepted!!!");
+        }
+        return a+b;}
+    }
+ 
